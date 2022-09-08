098725a0b31cf1f7df28e6602d59b2695767a3b0
willi <willi@Will_C_Dell.dhcp.wustl.edu>
2022-09-08 12:15:49 -0500

    Completed Stuido 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,15 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean xEqualsY = x==y;
+		boolean yEqualsZ = y==z;
+		boolean exception = xEqualsY==!yEqualsZ;
+		boolean xMoreY = x>y;
+		boolean yMoreZ = y>z;
+		boolean inc = !(exception)==(xMoreY==yMoreZ);
+//		
+//		boolean isOrdered = (x>y && y>z) || (z>y && y>x);
+		System.out.println(inc);
 	}
 
 }
